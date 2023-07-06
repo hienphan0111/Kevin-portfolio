6a19f43f2b133cabac81d7c8696d2643fd7dd915
hienphan0111 <hienphan@digitalcad.edu.vn>
2023-07-06 22:25:04 +0700

    add mobile style

--- app/layout.js
+++ app/layout.js
@@ -12,13 +12,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning={true} className="flex min-h-screen flex-col sm:py-0 md:py-10 items-center justify-center p-24 md:overflow-hidden bg-slate-500">
+      <body suppressHydrationWarning={true} className="flex min-h-screen flex-col sm:py-0 md:py-10 items-center md:justify-center sm:p-1 md:p-24 md:overflow-hidden bg-slate-500">
         <BackgroundAnimation />
-        <div className='relative flex flex-col lg:flex-row max-h-[680px]'>
+        <div className='sm:static md:relative flex flex-col lg:flex-row max-h-[680px]'>
           <div className='w-[100%] md:w-[70%] h-full flex flex-col'>
             <Hero />
           </div>
-          <div className='absolute top-0 -left-20'>
+          <div className='absolute top-0 md:-left-20'>
             <SideBar />
           </div>
           <div className='w-[60%] my-5 max-h-[600px] h-[600px] overflow-y-auto'>
