3e4cf33dd435a0ba1e5bfb970f36e77912b9bcb0
Hienphan0111 <hienphan@digitalcad.edu.vn>
2023-07-06 09:29:05 +0700

    Fix err

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -7,8 +7,8 @@ const About = () => {
         <div className="w-8 h-8 rounded-full bg-gradient-to-br from-yellow-400 to-yellow-50 absolute top-6 left-2 opacity-60"></div>
       </div>
       <div className="border-gradient-y py-5 px-7 text-gray-300">
-        <h2>Hello! I'm Kevin Phan.</h2>
-        <p>I'm a Full stack developer from Da Nang, Viet Nam. I am responsible for designing and building complete web application from start to finish. This includes creating user interfaces, writing server side code, and managing databases. With my comprehensive knowledge of the entire web development process. I can efficiently turn ides into fully functional websites or applications. </p>
+        <h2>Hello! I&apos;m Kevin Phan.</h2>
+        <p>I&apos;m a Full stack developer from Da Nang, Viet Nam. I am responsible for designing and building complete web application from start to finish. This includes creating user interfaces, writing server side code, and managing databases. With my comprehensive knowledge of the entire web development process. I can efficiently turn ides into fully functional websites or applications. </p>
       </div>
     </div>
   )
